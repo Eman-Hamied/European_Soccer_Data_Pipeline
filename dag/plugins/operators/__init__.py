dc7844c37c744756629994312d357ae768978d60
Eman Abd El-Hamied <eman.a.hamied@gmail.com>
2023-02-28 00:15:25 +0200

    Create __init__.py

---
+++ dag/plugins/operators/__init__.py
@@ -0,0 +1,11 @@
+from libs.data_scraping import DataScraping
+from libs.data_loading_trasforming import DataLoadingAndTransforming
+from libs.data_quality import DataQualityOperator
+
+
+__all__ = [
+    'DataScraping',
+	'DataLoadingAndTransforming',
+	'DataQualityOperator'
+	
+]
